7a94c15f0d7fc2f2b76c871c64eac42e6f574b65
pzi <91709633+pzistart@users.noreply.github.com>
2022-09-10 16:05:16 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -33,7 +33,7 @@ export const config = {
       // 想要发送的人的名字
       name: "铃",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oD34-5rNFCyi8tDu0gaJnP1p_c9w",
+      id: "oD34-5gACnwRYPl6ZAYKQPkrWjn8",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "rdNQgOk2ZE2x65nOFoUPoAf_ScAaFMphLypBojVZV7Q",
       // 所在省份
@@ -52,7 +52,7 @@ export const config = {
         {"type": "*生日", "name": "老婆", "year": "2000", "date": "02-20"},
 //         {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-//         {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
+        {"type": "生日", "name": "易", "year": "2000", "date": "10-19"},
 //         {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
