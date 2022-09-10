e51de9e2a14652e0619c7f8c1dd4e8006e525068
pzi <91709633+pzistart@users.noreply.github.com>
2022-09-10 16:18:36 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -33,7 +33,7 @@ export const config = {
       // 想要发送的人的名字
       name: "铃",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oD34-5gACnwRYPl6ZAYKQPkrWjn8",
+      id: "oD34-5rNFCyi8tDu0gaJnP1p_c9w",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "5XKKiZdzQVeACptvuUDMgV3hJ_KbSCPdGXi0M-PB708",
       // 所在省份
