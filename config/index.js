db79be77079d614707370544a01ac5b33d13bd81
pzi <91709633+pzistart@users.noreply.github.com>
2022-09-10 16:08:54 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -52,7 +52,7 @@ export const config = {
         {"type": "*生日", "name": "老婆", "year": "2000", "date": "02-20"},
 //         {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "name": "易", "year": "2000", "date": "10-19"},
+        {"type": "*生日", "name": "易", "year": "2000", "date": "10-19"},
 //         {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
@@ -74,15 +74,15 @@ export const config = {
       horoscopeDate: '03-25',
       horoscopeDateType: '今日',
       openUrl: "",
-            festivals: [
+      festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {"type": "*生日", "name": "老婆", "year": "2000", "date": "02-20"},
 //         {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "name": "易", "year": "2000", "date": "10-19"},
+        {"type": "*生日", "name": "易", "year": "2000", "date": "10-19"},
 //         {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
       ],
-            customizedDateList: [
+      customizedDateList: [
         // 在一起的日子
         {"keyword": "love_day", date: "2020-12-25"},
         // 
