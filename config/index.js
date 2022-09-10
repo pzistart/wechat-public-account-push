dd769e2e7f6c9111a9029b5cf83789d6ba2ec6eb
pzi <91709633+pzistart@users.noreply.github.com>
2022-09-10 14:44:26 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -35,7 +35,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oD34-5gACnwRYPl6ZAYKQPkrWjn8",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "_n6FImq8JMIub6Rzy8o9cstIBh3NJq_sD_cmo9dqM68",
+      useTemplateId: "ndw9loOm4PCG6pz1GQXFDXrRxdq0uPecjm-XlcOJAaE",
       // 所在省份
       province: "广东",
       // 所在城市
@@ -68,9 +68,9 @@ export const config = {
     {
       name: "yyp",
       id: "oD34-5sj7UMOfFjTRBMaYUAWZ1Pg",
-      useTemplateId: "",
+      useTemplateId: "ndw9loOm4PCG6pz1GQXFDXrRxdq0uPecjm-XlcOJAaE",
       province: "广东",
-      city: "工作",
+      city: "广州",
       horoscopeDate: '03-25',
       horoscopeDateType: '今日',
       openUrl: "",
